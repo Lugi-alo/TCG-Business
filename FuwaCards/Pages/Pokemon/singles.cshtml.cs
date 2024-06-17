8d03096a1db3a2e6941aba0206296ea57257976c
Lugi-alo <ejolohan@gmail.com>
2024-06-17 01:10:18 +0100

    added basic listing layout

---
+++ FuwaCards/Pages/Pokemon/singles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FuwaCards.Pages
+{
+    public class singlesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
