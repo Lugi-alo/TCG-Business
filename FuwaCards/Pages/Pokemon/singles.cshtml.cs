3f558a4a319c641f1afa4c7b3f502287cf2e45d7
Lugi-alo <ejolohan@gmail.com>
2024-06-18 00:29:48 +0100

    added filtering system for pokemon singles

--- FuwaCards/Pages/Pokemon/singles.cshtml.cs
+++ FuwaCards/Pages/Pokemon/singles.cshtml.cs
@@ -1,24 +1,38 @@
 using FuwaCards.Models;
+using FuwaCards.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FuwaCards.Pages
 {
     public class singlesModel : PageModel
     {
-		private readonly AppDataContext _context;
-		public IList<PokemonSingles> PokemonSinglesList { get; set; }
+        private readonly AppDataContext _context;
 
+        [BindProperty(SupportsGet = true)]
+        public PokemonSinglesFilter Filter { get; set; } = new PokemonSinglesFilter();
 
-		public singlesModel(AppDataContext context)
-		{
-			_context = context;
-		}
-		
-		public void OnGet()
+        public List<string> RarityOptions { get; set; } = new List<string>();
+
+        public singlesModel(AppDataContext context)
         {
-			PokemonSinglesList = _context.PokemonSingles.ToList();
-		}
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            await LoadDataAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await LoadDataAsync();
+            return Page();
+        }
 
         public async Task<IActionResult> OnPostDeleteAsync(int id)
         {
@@ -32,5 +46,37 @@ namespace FuwaCards.Pages
             return RedirectToPage("/Index");
         }
 
+        private async Task LoadDataAsync()
+        {
+            IQueryable<PokemonSingles> query = _context.PokemonSingles;
+
+            if (!string.IsNullOrEmpty(Filter.NameFilter))
+            {
+                var filter = Filter.NameFilter.ToUpper();
+                query = query.Where(s => s.Name.ToUpper().Contains(filter));
+            }
+
+            if (Filter.RaritySelection != null && Filter.RaritySelection.Any())
+            {
+                query = query.Where(s => Filter.RaritySelection.Contains(s.Rarity));
+            }
+
+            if (Filter.MinimumPriceFilter.HasValue)
+            {
+                query = query.Where(s => s.Price >= Filter.MinimumPriceFilter.Value);
+            }
+
+            if (Filter.MaximumPriceFilter.HasValue)
+            {
+                query = query.Where(s => s.Price <= Filter.MaximumPriceFilter.Value);
+            }
+
+            Filter.PokemonSinglesList = await query.ToListAsync();
+
+            RarityOptions = await _context.PokemonSingles
+                .Select(s => s.Rarity)
+                .Distinct()
+                .ToListAsync();
+        }
     }
 }
---
+++ FuwaCards/ViewModels/PokemonSinglesFilter.cs
@@ -0,0 +1,14 @@
+﻿using FuwaCards.Models;
+namespace FuwaCards.ViewModels
+{
+    public class PokemonSinglesFilter
+    {
+        public string NameFilter { get; set; }
+        public List<string> RaritySelection { get; set; }
+        public decimal? MinimumPriceFilter { get; set; }
+        public decimal? MaximumPriceFilter { get;set; }
+        public List<PokemonSingles> PokemonSinglesList { get; set; }
+
+
+    }
+}
