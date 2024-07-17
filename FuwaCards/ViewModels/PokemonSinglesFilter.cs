3dfeb9214b2d5df0146e48e1b10dedb6c689e523
Lugi-alo <ejolohan@gmail.com>
2024-07-17 14:25:46 +0100

    updated filter section

--- FuwaCards/ViewModels/PokemonSinglesFilter.cs
+++ FuwaCards/ViewModels/PokemonSinglesFilter.cs
@@ -4,12 +4,12 @@ namespace FuwaCards.ViewModels
     public class PokemonSinglesFilter
     {
         public string NameFilter { get; set; }
+        public Dictionary<string, int> RarityCounts { get; set; }
+        public Dictionary<string, int> SetNameCounts { get; set; }
         public List<string> RaritySelection { get; set; }
         public List<string> SetNameSelection { get; set; }
         public decimal? MinimumPriceFilter { get; set; }
         public decimal? MaximumPriceFilter { get;set; }
         public List<PokemonSingles> PokemonSinglesList { get; set; }
-
-
     }
 }
---
+++ FuwaCards/wwwroot/script/filter.js
@@ -0,0 +1,52 @@
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const handleDropdownToggle = (toggle) => {
+        const arrow = toggle.querySelector('.filter-arrow');
+        const dropdownId = toggle.getAttribute('data-toggle');
+        const dropdownContent = document.getElementById(dropdownId);
+
+        arrow.classList.toggle('open-filter');
+        dropdownContent.classList.toggle('show');
+
+        const isVisible = dropdownContent.classList.contains('show');
+
+        if (isVisible) {
+            dropdownContent.style.height = dropdownContent.scrollHeight + 'px';
+            dropdownContent.style.opacity = '1';
+        } else {
+            dropdownContent.style.height = '0';
+            dropdownContent.style.opacity = '0';
+
+            dropdownContent.addEventListener('transitionend', () => {
+                dropdownContent.style.height = '';
+            }, { once: true });
+        }
+    };
+
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+    dropdownToggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            handleDropdownToggle(toggle);
+        });
+    });
+
+    const handleCheckboxChange = (checkbox) => {
+        checkbox.closest('form').submit(); 
+    };
+
+    const checkboxes = document.querySelectorAll('.filter-container input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            handleCheckboxChange(checkbox);
+        });
+    });
+
+    resetCheckboxes();
+});
+
+
+function resetCheckboxes() {
+    const checkboxes = document.querySelectorAll('.dropdown-content input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+}
